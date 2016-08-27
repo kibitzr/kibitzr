e666d412c194d4125257c2d4431b92be797010ac
Demin, Petr <petr.demin@nih.gov>
2016-08-27 19:14:10 -0400

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='webwatcher',
-    version='0.2.0',
+    version='0.3.0',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
