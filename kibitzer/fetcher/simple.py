d63f93aac244ec634a84e9e8846fdd2f14b7ea2f
Demin, Petr <petr.demin@nih.gov>
2016-09-11 16:38:46 -0400

    Added retries to simple_fetcher

--- kibitzer/fetcher/simple.py
+++ kibitzer/fetcher/simple.py
@@ -1,12 +1,22 @@
+from time import sleep
 import requests
 
 from kibitzer import __version__ as version
 
 
 def simple_fetcher(conf):
-    response = requests.get(
-        url=conf['url'],
-        headers={'User-agent': 'Kibitzer/' + version},
-    )
-    ok = (response.status_code == 200)
-    return ok, response.text
+    retries = 3
+    for retry in range(retries):
+        try:
+            response = requests.get(
+                url=conf['url'],
+                headers={'User-agent': 'Kibitzer/' + version},
+            )
+        except requests.HTTPError:
+            if retry == retries - 1:
+                raise
+            else:
+                sleep(5)
+                continue
+        ok = (response.status_code == 200)
+        return ok, response.text
