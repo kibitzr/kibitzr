def86a95748d62a046eebe0ce3e123ded0b9c55e
Demin, Petr <petr.demin@nih.gov>
2016-09-14 09:17:43 -0400

    Bump version: 2.0.3 → 2.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='kibitzer',
-    version='2.0.3',
+    version='2.0.4',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
