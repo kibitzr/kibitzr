69cab21e1287e9d34274fcf59334db88ea26b715
Demin, Petr <petr.demin@nih.gov>
2016-09-17 17:10:12 -0400

    Bump version: 2.1.0 → 2.1.1

--- kibitzr/__init__.py
+++ kibitzr/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Peter Demin'
 __email__ = 'peterdemin@gmail.com'
-__version__ = '2.1.0'
+__version__ = '2.1.1'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='kibitzr',
-    version='2.1.0',
+    version='2.1.1',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
