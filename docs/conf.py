a48d8d0bb2555339811dc3cbae27b51e282edc8e
Petr <petr.demin@nih.gov>
2016-09-26 15:11:28 -0400

    Bump version: 2.2.1 → 2.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = u"2016, Peter Demin"
 # the built documents.
 #
 # The short X.Y version.
-version = '2.2.1'
+version = '2.3.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- kibitzr/__init__.py
+++ kibitzr/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Peter Demin'
 __email__ = 'peterdemin@gmail.com'
-__version__ = '2.2.1'
+__version__ = '2.3.0'
