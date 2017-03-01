8eb622c4fb27ed027ccdf628061c5e8d1ddaeacd
Demin, Petr <petr.demin@nih.gov>
2017-03-01 11:01:12 -0500

    Bump version: 2.5.0 → 2.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = u"2016, Peter Demin"
 # the built documents.
 #
 # The short X.Y version.
-version = '2.5.0'
+version = '2.5.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
