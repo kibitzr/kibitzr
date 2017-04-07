fe0752528353bdedd196ed16e6e40f008f5cd48b
Petr <petr.demin@nih.gov>
2017-04-07 11:23:00 -0400

    Added tests for main

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,14 @@
-class SettingsMock(object):
+from kibitzr.conf import ReloadableSettings
+
+
+class SettingsMock(ReloadableSettings):
+
     def __init__(self):
         self.pages = []
         self.notifiers = {}
         self.creds = {}
+
+    @classmethod
+    def instance(cls):
+        ReloadableSettings._instance = cls()
+        return ReloadableSettings._instance
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -4,18 +4,50 @@ from ..compat import mock
 from ..helpers import SettingsMock
 
 
-def test_main_executes_all_checks_before_loop():
-    settings = SettingsMock()
-    with mock.patch.object(main, "settings") as fake_settings:
-        fake_settings.return_value = settings
-        with mock.patch.object(main, "Checker") as checker:
-            main.main(once=True)
-    assert checker.call_count == 0  # hahaha
+@pytest.fixture(autouse=True)
+def settings():
+    """Override native settings singleton with empty one"""
+    SettingsMock.instance()
+
+
+@pytest.fixture(autouse=True)
+def check_noop(mocker):
+    """Cancel all side-effects from checks"""
+    mocker.patch("kibitzr.main.Checker.check", spec=True)
 
 
 def test_loop_aborts_without_checks():
-    settings = SettingsMock()
-    with mock.patch.object(main, "settings") as fake_settings:
-        fake_settings.return_value = settings
-        with pytest.raises(SystemExit):
-            main.main()
+    assert 1 == main.main()
+
+
+@mock.patch.object(main, "check_forever", side_effect=main.on_interrupt)
+def test_main_executes_all_checks_before_loop(the_loop):
+    main.settings().pages.append({
+        'name': 'A',
+        'script': {'python': 'ok, content = True, "ok"'}
+    })
+    assert 0 == main.main()
+    assert the_loop.call_count == 1
+    assert the_loop.call_args[0][0][0].check.call_count == 1
+
+
+def test_dummy_schedule():
+    main.Checker.check.side_effect = interrupt_on_nth_call(2)
+    main.settings().pages.append({
+        'name': 'A',
+        'script': {'python': 'ok, content = True, "ok"'},
+        'period': 0,
+    })
+    assert 0 == main.main()
+    assert main.Checker.check.call_count == 2
+
+
+def interrupt_on_nth_call(n):
+    def interrupter(*args, **kwargs):
+        if interrupter.n > 1:
+            interrupter.n -= 1
+        else:
+            main.on_interrupt()
+        return None
+    interrupter.n = n
+    return interrupter
