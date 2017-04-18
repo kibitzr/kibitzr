9e81012616b4a365af7c6244b5fb36531f53bf5c
Demin, Petr <petr.demin@nih.gov>
2017-04-18 17:27:43 -0400

    Bump version: 3.0.2 → 3.0.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def changelog_version():
 
 setup(
     name='kibitzr',
-    version='3.0.2',
+    version='3.0.3',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
