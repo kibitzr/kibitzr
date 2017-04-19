781bdfeb33b082744af615ddfa096cbb846d6fd2
Petr <petr.demin@nih.gov>
2017-04-19 10:29:52 -0400

    Bump version: 3.0.3 → 3.0.4

--- kibitzr/__init__.py
+++ kibitzr/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Peter Demin'
 __email__ = 'kibitzrrr@gmail.com'
-__version__ = '3.0.3'
+__version__ = '3.0.4'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def changelog_version():
 
 setup(
     name='kibitzr',
-    version='3.0.3',
+    version='3.0.4',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
