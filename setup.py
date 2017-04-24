70932225f0ab4f6cf2c6b98ceba5de33ed470c12
Petr <petr.demin@nih.gov>
2017-04-24 09:52:09 -0400

    Converted CLI to commands group

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'kibitzr=kibitzr.cli:entry'
+            'kibitzr=kibitzr.cli:cli'
         ]
     },
     include_package_data=True,
