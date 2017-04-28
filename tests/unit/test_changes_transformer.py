245ba0372a81256ef5fcd1f166438a477aff37a3
Demin, Petr <petr.demin@nih.gov>
2017-04-28 10:48:32 -0400

    Fuck pytest greedy fixture

--- tests/unit/test_changes_transformer.py
+++ tests/unit/test_changes_transformer.py
@@ -1,61 +1,57 @@
-import functools
+import contextlib
 import tempfile
 import shutil
 from kibitzr.storage import PageHistory
 
 
+@contextlib.contextmanager
 def history(style=None):
-    def wrapper(func):
-        @functools.wraps(func)
-        def inner():
-            storage_dir = tempfile.mkdtemp()
-            page_history = PageHistory(
-                conf={'name': 'test', 'url': 'web'},
-                storage_dir=storage_dir,
-                style=style,
-            )
-            try:
-                func(page_history)
-            finally:
-                shutil.rmtree(storage_dir)
-        return inner
-    return wrapper
-
-
-@history('default')
-def test_unified_diff_sample(page_history):
-    scenario = (
-        (u"hello", True, u"test at web\n@@ -0,0 +1 @@\n+hello"),
-        (u"world", True, u"test at web\n@@ -1 +1 @@\n-hello\n+world"),
-        (u"world", False, None),
+    storage_dir = tempfile.mkdtemp()
+    page_history = PageHistory(
+        conf={'name': 'test', 'url': 'web'},
+        storage_dir=storage_dir,
+        style=style,
     )
-    for content, changed, report in scenario:
-        result = page_history.report_changes(content)
-        assert result == (changed, report)
+    try:
+        yield page_history
+    finally:
+        shutil.rmtree(storage_dir)
 
 
-@history('verbose')
-def test_verbose_diff_sample(page_history):
-    scenario = (
-        (u"hello", True, u"test at web\nhello"),
-        (u"world", True, u"test at web\n"
-                         u"New value:\nworld\n"
-                         u"Old value:\nhello\n"),
-        (u"world", False, None),
-    )
-    for content, changed, report in scenario:
-        result = page_history.report_changes(content)
-        assert result == (changed, report)
+def test_unified_diff_sample():
+    with history('default') as page_history:
+        scenario = (
+            (u"hello", True, u"test at web\n@@ -0,0 +1 @@\n+hello"),
+            (u"world", True, u"test at web\n@@ -1 +1 @@\n-hello\n+world"),
+            (u"world", False, None),
+        )
+        for content, changed, report in scenario:
+            result = page_history.report_changes(content)
+            assert result == (changed, report)
 
 
-@history('word')
-def test_word_diff_sample(page_history):
-    scenario = (
-        (u"hello", True, u"hello\n"),
-        (u"world", True, u"[-hello-]{+world+}\n"
-                         u"last change was 0 seconds ago"),
-        (u"world", False, None),
-    )
-    for content, changed, report in scenario:
-        result = page_history.report_changes(content)
-        assert result == (changed, report)
+def test_verbose_diff_sample():
+    with history('verbose') as page_history:
+        scenario = (
+            (u"hello", True, u"test at web\nhello"),
+            (u"world", True, u"test at web\n"
+                             u"New value:\nworld\n"
+                             u"Old value:\nhello\n"),
+            (u"world", False, None),
+        )
+        for content, changed, report in scenario:
+            result = page_history.report_changes(content)
+            assert result == (changed, report)
+
+
+def test_word_diff_sample():
+    with history('word') as page_history:
+        scenario = (
+            (u"hello", True, u"hello\n"),
+            (u"world", True, u"[-hello-]{+world+}\n"
+                             u"last change was 0 seconds ago"),
+            (u"world", False, None),
+        )
+        for content, changed, report in scenario:
+            result = page_history.report_changes(content)
+            assert result == (changed, report)
