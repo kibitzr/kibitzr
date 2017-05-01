1367929da925ba2fc43fc227f9ebf2777a2a35a1
Peter Demin <kibitzrrr@gmail.com>
2017-04-30 21:37:54 -0400

    Jinja transform: Adding context and filters

--- kibitzr/transformer/jinja_transform.py
+++ kibitzr/transformer/jinja_transform.py
@@ -1,16 +1,72 @@
-from jinja2 import Template
+import json
+
+import six
+from jinja2 import Template, TemplateError
+from bs4 import BeautifulSoup
 
 from .utils import bake_parametrized
+from .html import deep_recursion, extract_text
 
 
 def jinja_transform(content, code, conf):
+    html = LazyHTML(content)
     context = {
         'conf': conf,
         'content': content,
         'lines': content.splitlines(),
+        'json': LazyJSON(content),
+        'css': html.css,
     }
     template = Template(code)
-    return True, template.render(context)
+    template.environment.filters['text'] = text_filter
+    try:
+        return True, template.render(context)
+    except TemplateError:
+        return False, None
+
+
+def text_filter(html):
+    if isinstance(html, list):
+        html = "".join(html)
+    ok, content = extract_text(html)
+    if ok:
+        return content
+    else:
+        raise RuntimeError("Extract text failed")
+
+
+class LazyJSON(object):
+    def __init__(self, content):
+        self.text = content
+        self._json = None
+
+    @property
+    def json(self):
+        if self._json is None:
+            self._json = json.loads(self.text)
+        return self._json
+
+    def __getitem__(self, key):
+        return self.json[key]
+
+
+class LazyHTML(object):
+    def __init__(self, content):
+        self.html = content
+        self._soup = None
+
+    @property
+    def soup(self):
+        if self._soup is None:
+            self._soup = BeautifulSoup(self.html, "html.parser")
+        return self._soup
+
+    def css(self, selector):
+        with deep_recursion():
+            elements = self.soup.select(selector)
+            result = [six.text_type(x)
+                      for x in elements]
+            return result
 
 
 JINJA_REGISTRY = {
--- tests/unit/test_jinja_transform.py
+++ tests/unit/test_jinja_transform.py
@@ -1,6 +1,29 @@
 from kibitzr.transformer.jinja_transform import jinja_transform
 
 
-def test_context_access():
-    jinja_transform('hello, world', 
+def test_content_is_passed():
+    ok, content = jinja_transform('world', 'hello {{ content }}!', {})
+    assert ok is True
+    assert content == "hello world!"
 
+
+def test_lines_are_passed():
+    ok, content = jinja_transform('a\nb', '{{ lines[1] }}', {})
+    assert ok is True
+    assert content == "b"
+
+
+def test_json_is_passed():
+    ok, content = jinja_transform('{"a": [1, 2, 3]}', '{{ json["a"][1] }}', {})
+    assert ok is True
+    assert content == "2"
+
+
+def test_css_selector_is_passed():
+    ok, content = jinja_transform(
+        '<div><a>A</a><p>P</p></div>',
+        '{{ css("div p") | text }}',
+        {},
+    )
+    assert ok is True
+    assert content == "P"
