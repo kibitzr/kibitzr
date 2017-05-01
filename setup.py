a92ba34adf5940565683a377dcfdbb929bf9ab27
Peter Demin <kibitzrrr@gmail.com>
2017-05-01 14:29:57 -0400

    Bump version: 3.1.1 → 3.1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def changelog_version():
 
 setup(
     name='kibitzr',
-    version='3.1.1',
+    version='3.1.2',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
