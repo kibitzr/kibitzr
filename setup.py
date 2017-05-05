ffa49293d5f0dc7d51f641cdf75d1ffc0fe45da2
Peter Demin <kibitzrrr@gmail.com>
2017-05-05 08:09:51 -0400

    4.0.0b0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def changelog_version():
 
 setup(
     name='kibitzr',
-    version='3.1.4',
+    version='4.0.0b0',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
