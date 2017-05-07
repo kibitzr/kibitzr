7da0513df0ccc2e37bd04755dd4e308194a96df4
Peter Demin <kibitzrrr@gmail.com>
2017-05-06 20:41:14 -0400

    Bump version: 3.1.6 → 3.1.7

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = u"2016, Peter Demin"
 # the built documents.
 #
 # The short X.Y version.
-version = '3.1.6'
+version = '3.1.7'
 # The full version, including alpha/beta/rc tags.
 release = version
 
