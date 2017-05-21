70de4e7a744f60f7ec7d1c412ac2b9766eb8ecbe
Peter Demin <kibitzrrr@gmail.com>
2017-05-21 19:35:40 -0400

    DRY

--- kibitzr/bash.py
+++ kibitzr/bash.py
@@ -3,6 +3,8 @@ import logging
 import tempfile
 import contextlib
 
+import six
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +18,9 @@ def execute_bash(code, stdin=None):
 
 
 class BashExecutor(object):
+
+    EXECUTABLE = "bash"
+    ARGS = []
 
     def __init__(self, code):
         self.code = code
@@ -37,41 +42,10 @@ class BashExecutor(object):
 
     @contextlib.contextmanager
     def temp_file(self):
-        with tempfile.NamedTemporaryFile() as fp:
-            logger.debug("Saving code to %r", fp.name)
-            fp.write(self.code.encode('utf-8'))
-            fp.flush()
-            yield fp.name
-
-    @staticmethod
-    def run_scipt(name, stdin):
-        from kibitzr.compat import sh
-        logger.debug("Launching script %r", name)
-        try:
-            return True, sh.Command("bash")(name, _in=stdin)
-        except sh.ErrorReturnCode as exc:
-            return False, exc
-
-    @staticmethod
-    def make_report(ok, result):
-        stdout = result.stdout.decode('utf-8')
-        stderr = result.stderr.decode('utf-8')
-        if ok:
-            log = logger.debug
-            report = stdout
-        else:
-            log = logger.error
-            report = stderr
-        log("Bash exit_code: %r", result.exit_code)
-        log("Bash stdout: %s", stdout)
-        log("Bash stderr: %s", stderr)
-        return ok, report
-
-
-class WindowsExecutor(BashExecutor):
-
-    @contextlib.contextmanager
-    def temp_file(self):
+        """
+        Create temporary file with code and yield its path.
+        Works both on Windows and Linux
+        """
         with tempfile.NamedTemporaryFile(suffix='.bat', delete=False) as fp:
             try:
                 logger.debug("Saving code to %r", fp.name)
@@ -81,27 +55,41 @@ class WindowsExecutor(BashExecutor):
             finally:
                 os.remove(fp.name)
 
-    @staticmethod
-    def run_scipt(name, stdin):
+    @classmethod
+    def run_scipt(cls, name, stdin):
         from kibitzr.compat import sh
         logger.debug("Launching script %r", name)
         try:
-            return True, sh.Command("cmd.exe")("/Q", "/C", name, _in=stdin)
+            args = cls.ARGS + [name]
+            return True, sh.Command(cls.EXECUTABLE)(*args, _in=stdin)
         except sh.ErrorReturnCode as exc:
             return False, exc
 
     @staticmethod
     def make_report(ok, result):
-        stdout = result.stdout
-        stderr = result.stderr
+        stdout = ensure_text(result.stdout)
+        stderr = ensure_text(result.stderr)
         if ok:
             log = logger.debug
             report = stdout
         else:
-            stdout = stdout.decode('utf-8')
-            stderr = stderr.decode('utf-8')
             log = logger.error
             report = stderr
-        log("CMD stdout: %s", stdout)
-        log("CMD stderr: %s", stderr)
+        if hasattr(result, 'exit_code'):
+            log("Command exit code: %r", result.exit_code)
+        log("Command stdout: %s", stdout)
+        log("Command stderr: %s", stderr)
         return ok, report
+
+
+class WindowsExecutor(BashExecutor):
+
+    EXECUTABLE = "cmd.exe"
+    ARGS = ["/Q", "/C"]
+
+
+def ensure_text(text):
+    if not isinstance(text, six.text_type):
+        return text.decode('utf-8')
+    else:
+        return text
