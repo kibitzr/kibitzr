59e95ba9935c714bacccb6c841c034991e630593
Peter Demin <kibitzrrr@gmail.com>
2017-05-25 14:54:07 -0400

    Bump version: 4.0.2 → 4.0.3

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ else:
 
 setup(
     name='kibitzr',
-    version='4.0.2',
+    version='4.0.3',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
