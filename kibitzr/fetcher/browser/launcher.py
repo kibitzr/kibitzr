40670228554062c87d522d5c56c894d2c19e5711
Peter Demin <poslano@gmail.com>
2017-12-16 21:45:11 -0500

    Simplified Firefox launcher

--- kibitzr/fetcher/browser/launcher.py
+++ kibitzr/fetcher/browser/launcher.py
@@ -12,34 +12,25 @@ from contextlib import contextmanager
 PROFILE_DIR = 'firefox_profile'
 logger = logging.getLogger(__name__)
 FIREFOX_INSTANCE = {
-    'driver': None,
-    'headed_driver': None,
+    'headless': None,
+    'headed': None,
 }
 
 
 def cleanup():
     """Must be called before exit"""
     temp_dirs = []
-    if FIREFOX_INSTANCE['driver'] is not None:
-        if FIREFOX_INSTANCE['driver'].profile:
-            temp_dirs.append(FIREFOX_INSTANCE['driver'].profile.profile_dir)
-        try:
-            FIREFOX_INSTANCE['driver'].quit()
-            FIREFOX_INSTANCE['driver'] = None
-        except:
-            logger.exception(
-                "Exception occurred in browser cleanup"
-            )
-    if FIREFOX_INSTANCE['headed_driver'] is not None:
-        if FIREFOX_INSTANCE['headed_driver'].profile:
-            temp_dirs.append(FIREFOX_INSTANCE['headed_driver'].profile.profile_dir)
-        try:
-            FIREFOX_INSTANCE['headed_driver'].quit()
-            FIREFOX_INSTANCE['headed_driver'] = None
-        except:
-            logger.exception(
-                "Exception occurred in browser cleanup"
-            )
+    for key in ('headless', 'headed'):
+        if FIREFOX_INSTANCE[key] is not None:
+            if FIREFOX_INSTANCE[key].profile:
+                temp_dirs.append(FIREFOX_INSTANCE[key].profile.profile_dir)
+            try:
+                FIREFOX_INSTANCE[key].quit()
+                FIREFOX_INSTANCE[key] = None
+            except:
+                logger.exception(
+                    "Exception occurred in browser cleanup"
+                )
     for temp_dir in temp_dirs:
         shutil.rmtree(temp_dir, ignore_errors=True)
 
@@ -51,25 +42,22 @@ def firefox(headless=True):
     Instance is reused and must be cleaned up on exit.
     """
     from selenium import webdriver
-    from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+    from selenium.webdriver.firefox.options import Options
     if headless:
-        driver_key = 'driver'
+        driver_key = 'headless'
+        firefox_options = Options()
+        firefox_options.add_argument('-headless')
     else:
-        driver_key = 'headed_driver'
+        driver_key = 'headed'
+        firefox_options = None
+    # Load profile, if it exists:
+    if os.path.isdir(PROFILE_DIR):
+        firefox_profile = webdriver.FirefoxProfile(PROFILE_DIR)
+    else:
+        firefox_profile = None
     if FIREFOX_INSTANCE[driver_key] is None:
-        if logger.level == logging.DEBUG:
-            firefox_binary = FirefoxBinary(log_file=sys.stdout)
-        else:
-            firefox_binary = FirefoxBinary()
-        if headless:
-            firefox_binary.add_command_line_options('-headless')
-        # Load profile, if it exists:
-        if os.path.isdir(PROFILE_DIR):
-            firefox_profile = webdriver.FirefoxProfile(PROFILE_DIR)
-        else:
-            firefox_profile = None
         FIREFOX_INSTANCE[driver_key] = webdriver.Firefox(
-            firefox_binary=firefox_binary,
             firefox_profile=firefox_profile,
+            firefox_options=firefox_options,
         )
     yield FIREFOX_INSTANCE[driver_key]
