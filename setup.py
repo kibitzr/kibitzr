3437aedc21cfeffc3526b8c86b4122b70c4ab36c
Peter Demin <kibitzrrr@gmail.com>
2018-05-08 17:10:48 -0400

    bump version to 5.3.0alpha1

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ else:
 
 setup(
     name='kibitzr',
-    version='5.3.0.alpha',
+    version='5.3.0.alpha1',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
