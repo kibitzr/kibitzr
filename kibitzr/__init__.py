8f7e7115671759ea86acdd8d66ad1334d05abb47
Peter Demin <kibitzrrr@gmail.com>
2018-10-02 11:32:47 -0400

    Bump version: 5.3.4 → 5.3.5

--- kibitzr/__init__.py
+++ kibitzr/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Peter Demin'
 __email__ = 'kibitzrrr@gmail.com'
-__version__ = '5.3.4'
+__version__ = '5.3.5'
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name='kibitzr',
-    version='5.3.4',
+    version='5.3.5',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
