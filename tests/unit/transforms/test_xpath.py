8db3afb1e9a7f9a7f48b5a92b9c08c9df6b1680e
Maciek Starzyk <mstarzyk@gmail.com>
2019-06-01 18:44:46 +0200

    Parametrized xpath tests

--- tests/unit/transforms/test_xpath.py
+++ tests/unit/transforms/test_xpath.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from .helpers import run_transform, HTML
 
 
-def test_xpath_selector():
-    ok, content = run_transform('xpath', '//body/h2/a[@id="page-link"]', HTML)
+@pytest.mark.parametrize("selector,expected", [
+    pytest.param('boolean(//body/h2/a)', 'True', id='boolean'),
+    pytest.param("count(//div)", "2.0", id="float"),
+    pytest.param("string(//a)", "Page", id="string"),
+    pytest.param("//body/h2/a/@href", "page.html", id="attribute"),
+    pytest.param(
+        '//body/h2/a[@id="page-link"]',
+        '<a href="page.html" id="page-link">Page</a>',
+        id='element'
+    ),
+    pytest.param(
+        '/html/namespace::*[name()]',
+        'xml="http://www.w3.org/XML/1998/namespace"',
+        id='namespace'
+    ),
+])
+def test_xpath_selector(selector, expected):
+    ok, content = run_transform('xpath', selector, HTML)
     assert ok is True
-    assert content == '<a href="page.html" id="page-link">Page</a>'
+    assert content == expected
 
 
 def test_xpath_selector_all():
@@ -15,27 +33,3 @@ def test_xpath_selector_all():
         u'<div id="content"> Привет, Мир! </div>',
         u'<div class="footer"> Footer content </div>',
     ])
-
-
-def test_xpath_selector_boolean():
-    ok, content = run_transform('xpath', 'boolean(//body/h2/a)', HTML)
-    assert ok is True
-    assert content == 'True'
-
-
-def test_xpath_selector_float():
-    ok, content = run_transform('xpath', 'count(//div)', HTML)
-    assert ok is True
-    assert content == '2.0'
-
-
-def test_xpath_selector_attribute():
-    ok, content = run_transform('xpath', '//body/h2/a/@href', HTML)
-    assert ok is True
-    assert content == 'page.html'
-
-
-def test_xpath_selector_namespace():
-    ok, content = run_transform('xpath', '/html/namespace::*[name()]', HTML)
-    assert ok is True
-    assert content == 'xml="http://www.w3.org/XML/1998/namespace"'
