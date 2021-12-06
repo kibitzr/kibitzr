96f160350da86eb77db61f5a43eb7f4d75dba6c4
Peter Demin <peter.demin@toptal.com>
2021-12-05 19:34:17 -0500

    Bump version: 6.0.2 → 6.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = u"2016, Peter Demin"
 # the built documents.
 #
 # The short X.Y version.
-version = '6.0.2'
+version = '6.0.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name='kibitzr',
-    version='6.0.2',
+    version='6.0.3',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
