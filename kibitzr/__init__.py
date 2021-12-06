f4b73d92be9247bf88036b4d0fdeaac9acbb8301
Peter Demin <peter.demin@toptal.com>
2021-12-06 09:41:04 -0500

    Bump version: 6.0.3 → 6.0.4

--- kibitzr/__init__.py
+++ kibitzr/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Peter Demin'
 __email__ = 'kibitzrrr@gmail.com'
-__version__ = '6.0.3'
+__version__ = '6.0.4'
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name='kibitzr',
-    version='6.0.3',
+    version='6.0.4',
     description="Self hosted web page changes monitoring",
     long_description=readme + '\n\n' + history,
     author="Peter Demin",
